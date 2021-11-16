cabb2f2e021a67ca41e47f0949ec54ea9706b76e
Nada <81916000+Nada0088@users.noreply.github.com>
2021-11-16 20:52:01 +0100

    Update reseau de neuron.py

--- reseau de neuron.py
+++ reseau de neuron.py
@@ -14,8 +14,7 @@ max_dimension : 600
 feature_extractor {
 type : 'faster_rcnn_inception_v2'
 
-#denir à quel point on veux approfondir la convolution pour extraire les fonction-
-nalités
+#denir à quel point on veux approfondir la convolution pour extraire les fonctionnalités
 
 rst_stage_features_stride : 16
 }
@@ -34,17 +33,13 @@ scales : [0.25, 0.5,0.75, 1.0, 1.5, 2.0]
 }
 #La régularisation du poids fournit une approche permettant de réduire
 #la sur adaptation d un modèle du CNN sur les données d entraînement
-#et d améliorer les performances du modèle sur de nouvelles données, telles que l
-ensemble de tests de maintien
+#et d améliorer les performances du modèle sur de nouvelles données, telles que lensemble de tests de maintien
 #la régularisation L2 regles logistiques et le model de réseaux de neurones
-#Il existe deux paramètres de régularisation courants :L1 s appelle Lasso et L2 s
-appelle Ridge
-#Ils ont utilisé la perte L1 au lieu de la perte L2 car les valeurs de la tête de régression
-prévue
-#de la RPN ne sont pas bornées. La perte de régression est également appliquée aux
-boîtes englobantes qui ont une étiquette positive
-#Un modèle avec des poids élevés est plus complexe qu un modèle avec des poids
-plus petits
+#Il existe deux paramètres de régularisation courants :L1 s appelle Lasso et L2 sappelle Ridge
+#Ils ont utilisé la perte L1 au lieu de la perte L2 car les valeurs de la tête de régression prévue
+#de la RPN ne sont pas bornées. La perte de régression est également appliquée aux 
+#boîtes englobantes qui ont une étiquette positive
+#Un modèle avec des poids élevés est plus complexe qu un modèle avec des poids plus petits
 rst_stage_box_predictor_conv_hyperparams {
 op : CONV
 regularizer {
@@ -54,10 +49,8 @@ weight : 0.0005
 }
 #Les initialisations dénissent la manière de dénir les poids aléatoires initiaux
 initializer {
-#Le but de l'utilisation de la normale tronquée est de surmonter la saturation des
-fonctions
-#temporelles comme sigmoïde (où si la valeur est trop grande / petite, le neurone
-arrête d'apprendre).
+#Le but de l'utilisation de la normale tronquée est de surmonter la saturation des fonctions
+#temporelles comme sigmoïde (où si la valeur est trop grande / petite, le neurone arrête d'apprendre).
 truncated_normal_initializer {
 #Écart type des valeurs aléatoires à générer.
 stddev : 0.01
@@ -76,11 +69,9 @@ rst_stage_max_proposals : 100
 #Les modèles de détection dénissent généralement un poids de perte pour la clas-
 sication et l objectivité.
 
-#Cette fonction met à jour les poids de sorte que le rapport entre le poids de clas-
-sication et le poids
+#Cette fonction met à jour les poids de sorte que le rapport entre le poids de classication et le poids
 
-#de localisation soit le rapport fourni. De manière arbitraire, le poids de localisation
-est déni sur 1.0.
+#de localisation soit le rapport fourni. De manière arbitraire, le poids de localisation est déni sur 1.0.
 rst_stage_localization_loss_weight : 2.0
 rst_stage_objectness_loss_weight : 1.0
 #14*14 pour inception v2
@@ -92,18 +83,15 @@ maxpool_stride : 2
 #de haut niveau et produisent deux prédictions,
 #1 un emplacement de case de codage tenseur et
 #2 une classe de codage tenseur pour chaque case.
-#Ces composants sont directement transmis aux fonctions de perte dans nos modèles
-de détection.
+#Ces composants sont directement transmis aux fonctions de perte dans nos modèles de détection.
 
-#Ces modules sont séparés du modèle principal car les mêmes architectures de pré-
-dicteurs de boîtes sont partagées entre plusieurs modèles.
+#Ces modules sont séparés du modèle principal car les mêmes architectures 
+#de prédicteurs de boîtes sont partagées entre plusieurs modèles.
 
-#mask box predictor pour chaque ancre, une prédiction distincte est eectuée pour
-chaque classe.
-#Outre la prédiction de boîtes et de classes, cette classe permet également de prévoir
-des masques
+#mask box predictor pour chaque ancre, une prédiction distincte est eectuée pour chaque classe.
+#Outre la prédiction de boîtes et de classes, cette classe permet également de prévoir des masques
 #et des points-clés dans des boîtes de détection. Actuellement, ce prédicteur de case
-établit des prédictions par classe.
+#établit des prédictions par classe.
 #c a d que chaque ancre fait une prédiction de boîte distincte pour chaque classe.
 second_stage_box_predictor {
 mask_rcnn_box_predictor {
@@ -166,16 +154,14 @@ gradient_clipping_by_norm : 10.0
 # chemin d'accès au point contrôle pré-existant
 ne_tune_checkpoint : "/home/nada/Desktop/training/my_models/Faster_inc_v2
 ]
-#valeur booléenne si false : le point de contrôle est celui d une classication sinon
-detection.
+#valeur booléenne si false : le point de contrôle est celui d une classication sinon detection.
 from_detection_checkpoint : true
 num_steps : 30000
 }
 #qui détermines quel ensemble de métriques seront rapporte pour l'évaluation.
 eval_cong {
 
-#le nombre de lots (batchs)(actuellement de taille 1) utilise pour un cycle d'évalua-
-tion
+#le nombre de lots (batchs)(actuellement de taille 1) utilise pour un cycle d'évaluation
 
 #et correspond souvent a la taille totale du jeu de donnée d'évaluation
 num_examples : 119
